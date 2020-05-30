9efe3caf588834c9ba6b6211c69c309b897c7206
Jia Jia <angersax@sina.com>
2020-05-30 11:54:58 +0800

    Refactor code

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-DOCKER=full-docker
 NAME=craftslab/theiadocker
 TAG=latest
 
-pushd $DOCKER
 docker build --no-cache -f Dockerfile -t $NAME:$TAG .
-popd
-
 #sudo docker inspect $NAME:$TAG > Dockerobjects
